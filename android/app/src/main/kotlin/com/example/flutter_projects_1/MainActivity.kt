d67c719b15dd2205c107defdeaa607677ba73b06
Farhaan sheriff <farhaansheriff715@gmail.com>
2026-01-08 20:45:53 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/flutter_projects_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_projects_1/MainActivity.kt
@@ -1,5 +1,34 @@
-package com.example.flutter_projects_1
+package com.example.flutter_projects_internship
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "device_info_channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+
+            when (call.method) {
+
+                "getDeviceInfo" -> {
+                    val info = mapOf(
+                        "model" to Build.MODEL,
+                        "androidVersion" to Build.VERSION.RELEASE
+                    )
+                    result.success(info)
+                }
+
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
